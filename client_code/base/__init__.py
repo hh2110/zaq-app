285c8f0ae9c1e53ed28cbfb5ae3ec318ea6d5ecd
hikmat@abaseen.org <hikmat@abaseen.org>
2023-11-07 21:20:43 +0000

    Created add_data and total_donations.png; Edited settings, base and home

--- client_code/base/__init__.py
+++ client_code/base/__init__.py
@@ -8,15 +8,60 @@ from anvil.tables import app_tables
 import anvil.users
 
 from ..home import home
+from ..add_data import add_data
 
 class base(baseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.change_sign_in_text()
     self.content_panel.add_component(home())
+    self.toggle_content_panel_visiblity()
 
-    # Any code you write here will run before the form opens.
+  def toggle_content_panel_visiblity(self):
+    self.content_panel.visible = anvil.users.get_user() is not None
+
+  def go_to_page(self, target):
+    self.content_panel.clear()
+    self.content_panel.add_component(target)
+
+  def change_sign_in_text(self):
+    user = anvil.users.get_user()
+    if user:
+      self.sign_in.text = user["email"]
+    else:
+      self.sign_in.text = "Sign In"
 
   def add_data_click(self, **event_args):
     """This method is called when the link is clicked"""
-    pass
+    self.go_to_page(target=add_data())
+
+  def dashboard_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.go_to_page(target=home())
+
+  def view_data_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.go_to_page(target=home())
+
+  def reports_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.go_to_page(target=home())
+
+  def title_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.go_to_page(target=home())
+
+  def sign_in_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    user = anvil.users.get_user()
+    if user:
+      logout = confirm("Would you like to logout?")
+      if logout:
+        anvil.users.logout()
+        self.go_to_page(target=home())
+    else:
+      anvil.users.login_with_form()
+    
+    self.change_sign_in_text()
+    self.toggle_content_panel_visiblity()
